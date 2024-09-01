16e1125ca1fe7994d631f5d9a82931f1e77e7ede
ckh3296 <ckh3296@autuni.ac.nz>
2024-09-01 12:00:58 +1200

    Finished TODO List

--- lib/pages/main_pages/task_page.dart
+++ lib/pages/main_pages/task_page.dart
@@ -1,4 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'adding_task_page.dart';
+
+class Task {
+  final String title;
+  final String type;
+  final String? date;
+  final String description;
+
+  Task({
+    required this.title,
+    required this.type,
+    this.date,
+    required this.description,
+  });
+
+  factory Task.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Task(
+      title: data['title'],
+      type: data['type'],
+      date: data['date'],
+      description: data['description'],
+    );
+  }
+}
 
 class TaskPage extends StatefulWidget {
   const TaskPage({super.key});
@@ -8,10 +35,89 @@ class TaskPage extends StatefulWidget {
 }
 
 class _TaskPageState extends State<TaskPage> {
+  List<Task> dailyTasks = [];
+  List<Task> weeklyTasks = [];
+  List<Task> monthlyTasks = [];
 
-  int number_of_daily = 1;
-  int number_of_weekly = 1;
-  int number_of_monthly = 1;
+  @override
+  void initState() {
+    super.initState();
+    _loadTasks();
+  }
+
+  // request login user's data from firebase
+  void _loadTasks() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      final userEmail = user.email;
+
+      FirebaseFirestore.instance
+          .collection('tasks')
+          .where('owner', isEqualTo: userEmail)
+          .snapshots()
+          .listen((snapshot) {
+        setState(() {
+          dailyTasks.clear();
+          weeklyTasks.clear();
+          monthlyTasks.clear();
+
+          for (var doc in snapshot.docs) {
+            var task = Task.fromFirestore(doc);
+
+            if (task.type == 'Daily') {
+              dailyTasks.add(task);
+            } else if (task.type == 'Weekly') {
+              weeklyTasks.add(task);
+            } else if (task.type == 'Monthly') {
+              monthlyTasks.add(task);
+            }
+          }
+        });
+      });
+    }
+  }
+
+  // Add Task
+  void _addTask(Task task) {
+    setState(() {
+      if (task.type == 'Daily') {
+        dailyTasks.add(task);
+      } else if (task.type == 'Weekly') {
+        weeklyTasks.add(task);
+      } else if (task.type == 'Monthly') {
+        monthlyTasks.add(task);
+      }
+    });
+  }
+
+  // Dialog that shows details of the task
+  void _showTaskDetails(BuildContext context, Task task) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(task.title),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Date: ${task.date ?? 'No date selected'}"),
+              SizedBox(height: 10),
+              Text("Description: ${task.description}"),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,84 +130,70 @@ class _TaskPageState extends State<TaskPage> {
         ),
         actions: [
           IconButton(
-              onPressed: () => {},
-              icon: Icon(Icons.calendar_today_rounded)
+            onPressed: () => {},
+            icon: Icon(Icons.calendar_today_rounded),
           ),
           IconButton(
-              onPressed: () => {},
-              icon: Icon(Icons.add)
+            onPressed: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => AddingTaskPage(),
+                ),
+              );
+              if (result != null && result is Task) {
+                _addTask(result);
+              }
+            },
+            icon: Icon(Icons.add),
           )
         ],
       ),
       body: ListView(
         children: [
+          // Daily Tasks Section
+          _buildTaskSection("Daily Task", dailyTasks),
+
+          // Weekly Tasks Section
+          _buildTaskSection("Weekly Task", weeklyTasks),
+
+          // Monthly Tasks Section
+          _buildTaskSection("Monthly Task", monthlyTasks),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTaskSection(String title, List<Task> tasks) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(10),
-                  child: Text("Daily Task"),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Theme.of(context).colorScheme.inversePrimary
-                      )
-                  ),
-                  margin: EdgeInsets.all(10),
-                  width: 400, height: number_of_daily * 50,
-                  child: Center(
-                      child: Text("There is no task now T^T")
-                  ),
-                ),
-              ],
-            ),
+            margin: EdgeInsets.all(10),
+            child: Text(title),
           ),
           Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(10),
-                  child: Text("Weekly Task"),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Theme.of(context).colorScheme.inversePrimary
-                      )
-                  ),
-                  margin: EdgeInsets.all(10),
-                  width: 400, height: number_of_weekly * 50,
-                  child: Center(
-                        child: Text("There is no task now T^T")
-                  ),
-                ),
-              ],
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
             ),
-          ),
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.all(10),
-                  child: Text("Monthly Task"),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Theme.of(context).colorScheme.inversePrimary
-                      )
-                  ),
-                  margin: EdgeInsets.all(10),
-                  width: 400, height: number_of_monthly * 50,
-                  child: Center(
-                      child: Text("There is no task now T^T")
-                  ),
-                ),
-              ],
+            margin: EdgeInsets.all(10),
+            width: 400,
+            child: tasks.isNotEmpty
+                ? Column(
+              children: tasks.map((task) {
+                return ListTile(
+                  title: Text(task.title),
+                  onTap: () {
+                    _showTaskDetails(context, task);
+                  },
+                );
+              }).toList(),
+            )
+                : Center(
+              child: Text("There is no task now T^T"),
             ),
           ),
         ],

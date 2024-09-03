054494936f6692f9ef7e155e03059eb104269a5a
NozoYou <33321720+NozoYou@users.noreply.github.com>
2024-09-03 19:35:37 +1200

    Change register_page.dart Update userName and userAvatar when finished register.

--- lib/pages/message_pages/messages_page.dart
+++ lib/pages/message_pages/messages_page.dart
@@ -1,4 +1,3 @@
-import 'package:all_in_one/pages/message_pages/group_chat_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -143,8 +142,6 @@ class _MessagesPageState extends State<MessagesPage> {
           IconButton(
             icon: Icon(Icons.group),
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => GroupChatPage()));
             },
           ),
         ],
